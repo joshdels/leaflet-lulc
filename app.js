26cb96ded14dd9436c1f3e1646d1e50e3a6c0e87
joshdels <deleonjoshy2@gmail.com>
2025-09-22 21:25:04 +0800

    connect all need styling and more understanding

--- app.js
+++ app.js
@@ -1,84 +1,115 @@
-var map = L.map('map');
+var map = L.map("map", {
+  attributionControl: false,
+  zoomControl: false,
+});
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 22,
 }).addTo(map);
 
-// var marker = L.marker([51.5, -0.09]).addTo(map);
+L.control
+  .zoom({
+    position: "topright",
+  })
+  .addTo(map);
 
-// map.fitBounds(L.featureGroup([marker]).getBounds(), {
-//   maxZoom: 8,
-// });
+// Responsiveness
+map.createPane("lulcPane");
+map.getPane("lulcPane").style.zIndex = 200;
 
+map.createPane("boundaryPane");
+map.getPane("boundaryPane").style.zIndex = 300;
 
 // Geoservered Files
-// var geoserverUrl = 'http://ows.mundialis.de/services/service?';
-// var layerName = "TOPO-WMS";
+var boundary = null;
+var lulc = null;
+var lulcData = null;
+var results = [];
 
-// // 2. Add the WMS layer
-// var wmsLayer = L.tileLayer.wms(geoserverUrl, {
-//   layers: layerName,
-//   format: "image/png",
-//   transparent: true,
-//   attribution: "Mundialis WMS"
-// }).addTo(map);
+var geoserverUrl = "http://localhost:8080/geoserver/josh/wfs";
 
-/////// Spatial Analysis
-// Example point (Manila City Hall)
-var point = turf.point([120.9842, 14.5995]);
-
-// Buffer the point (1 km)
-var buffered = turf.buffer(point, 2, { units: 'kilometers' });
-
-// Convert GeoJSON to Leaflet layers
-var pointLayer = L.geoJSON(point).addTo(map);
-var bufferLayer = L.geoJSON(buffered, {
-  style: { color: 'red', fillOpacity: 0.2 }
-}).addTo(map);
-
-// Zoom to the buffer
-// map.fitBounds(bufferLayer.getBounds());
+// STEP 1: load lulc first
+fetch(
+  geoserverUrl +
+    "?service=WFS&version=1.0.0&request=GetFeature&typeName=josh:lulc_clipped&outputFormat=application/json"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    lulcData = data;
+    lulc = L.geoJSON(data, {
+      pane: "lulcPane",
+      style: { color: "red", weight: 0.5 },
+    }).addTo(map);
 
 
-///////
-// // Turf distance
-// var p1 = turf.point([120.9842, 14.5995]);
-// var p2 = turf.point([121.0437, 14.6760]);
+    return fetch(
+      geoserverUrl +
+        "?service=WFS&version=1.0.0&request=GetFeature&typeName=josh:tagum_adm4&outputFormat=application/json"
+    );
+  })
+  .then((res) => res.json())
+  .then((data) => {
+    boundary = L.geoJSON(data, {
+      pane: "boundaryPane",
+      style: {
+        color: "blue",
+        weight: 2,
+        fillOpacity: 0,
+      },
+      onEachFeature: function (feature, layer) {
+        layer.on("click", function () {
+          let barangay = feature.properties.adm4_en;
+          let filtered = handleFilterLulc(barangay);
+          updateChart(filtered, barangay);
+        });
+      },
+    }).addTo(map);
 
-// var distance = turf.distance(p1, p2, { units: 'kilometers' });
-// console.log("Distance:", distance, "km");
+    map.fitBounds(boundary.getBounds());
+  });
 
-// //////
-// // polygons
-var poly1 = turf.polygon([
-  [
-    [-122.801742, 45.48565],
-    [-122.801742, 45.60491],
-    [-122.584762, 45.60491],
-    [-122.584762, 45.48565],
-    [-122.801742, 45.48565],
-  ],
-]);
-
-var poly2 = turf.polygon([
-  [
-    [-122.520217, 45.535693],
-    [-122.64038, 45.553967],
-    [-122.720031, 45.526554],
-    [-122.669906, 45.507309],
-    [-122.723464, 45.446643],
-    [-122.532577, 45.408574],
-    [-122.487258, 45.477466],
-    [-122.520217, 45.535693],
-  ],
-]);
-
-var intersection = turf.intersect(turf.featureCollection([poly1, poly2]));
-
-var polygons = L.geoJSON([poly1, poly2], { style: { color:'blue' }}).addTo(map);
-map.fitBounds(polygons.getBounds());
-if (intersection) {
-  var intersectionLayer = L.geoJSON(intersection, { style: { color: 'green' }}).addTo(map);
-} else {
-  console.log("No intersect found");
+// Filter queries
+function handleFilterLulc(boundary) {
+  results = [];
+  lulcData.features.forEach((feature, i) => {
+    let prop = feature.properties;
+    if (boundary === prop.adm4_en) {
+      results.push(prop);
+    } 
+  });
+  return results;
 }
+
+
+///// Chart
+
+const labels = results.map(p => p.class_name);
+const values = results.map(p => p.area_ha);
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+  type: 'bar',              
+  data: {
+    labels: labels, 
+    datasets: [{
+      label: 'Land Use Area',
+      data: values,    
+      backgroundColor: 'rgba(0,0,0,0.1)', 
+      borderColor: 'rgba(0,0,0,0.8)',
+      borderWidth: 1
+    }]
+  },
+  options: { 
+    responsive: true,
+    maintainAspectRatio: false,
+   }
+});
+
+// Update chart dynamically
+function updateChart(dataArray, barangay) {
+  myChart.data.labels = dataArray.map(p => p.class_name);
+  myChart.data.datasets[0].data = dataArray.map(p => p.area_ha);
+  myChart.data.datasets[0].label = `${barangay}'s Land Use Area`;
+  myChart.update();
+}
+
