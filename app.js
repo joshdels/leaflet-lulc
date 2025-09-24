3b45dac5b5b97d289431f77e6ab4a15c75830b8e
joshdels <deleonjoshy2@gmail.com>
2025-09-24 22:28:43 +0800

    done geoportal draft

--- app.js
+++ app.js
@@ -28,7 +28,57 @@ var results = [];
 
 var geoserverUrl = "http://localhost:8080/geoserver/josh/wfs";
 
-// STEP 1: load lulc first
+function handleStyles(object) {
+  var color;
+  var fillColor;
+
+  switch(object) {
+    case "Built-up":
+      color = '#e74c3c';
+      fillColor = '#e74c3c';
+      break;
+    case "Open/Barren":
+      color = '#c8cdd1';
+      fillColor = '#c8cdd1'
+      break;
+    case "Inland Water":
+      color = '#1a27e7';
+      fillColor = '#1a27e7'
+      break;
+    case "Brush/Shrubs":
+      color = '#00ec33';
+      fillColor = '#00ec33'
+      break;
+    case "Grassland":
+      color = '#dfa335';
+      fillColor = '#dfa335'
+      break;
+    case "Perennial Crop":
+      color = '#fbff00';
+      fillColor = '#fbff00'
+      break;
+    case "Annual Crop":
+      color = '#12c42a';
+      fillColor = '#12c42a'
+      break;
+    case "Fishpond":
+      color = '#0400fa';
+      fillColor = '#0400fa'
+      break;
+    case "Open Forest":
+      color = '#126426';
+      fillColor = '#126426'
+      break;
+    default:
+      color = '#808b96';
+      fillColor = '#808b96'
+      break;
+  }
+
+  return {color:color, fillColor:fillColor, fillOpacity: 1}
+}
+
+// Data
 fetch(
   geoserverUrl +
     "?service=WFS&version=1.0.0&request=GetFeature&typeName=josh:lulc_clipped&outputFormat=application/json"
@@ -36,9 +86,13 @@ fetch(
   .then((res) => res.json())
   .then((data) => {
     lulcData = data;
+    console.log(data.features),
+
     lulc = L.geoJSON(data, {
       pane: "lulcPane",
-      style: { color: "red", weight: 0.5 },
+      style: function (feature) {
+        return handleStyles(feature.properties.class_name)
+      }
     }).addTo(map);
 
 
@@ -52,7 +106,7 @@ fetch(
     boundary = L.geoJSON(data, {
       pane: "boundaryPane",
       style: {
-        color: "blue",
+        color: "black",
         weight: 2,
         fillOpacity: 0,
       },
@@ -63,6 +117,7 @@ fetch(
           updateChart(filtered, barangay);
         });
       },
+      
     }).addTo(map);
 
     map.fitBounds(boundary.getBounds());
@@ -82,9 +137,9 @@ function handleFilterLulc(boundary) {
 
 
 ///// Chart
-
 const labels = results.map(p => p.class_name);
 const values = results.map(p => p.area_ha);
+const barColors =results.map(p => handleStyles(p.class_name).fillColor)
 
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
@@ -94,14 +149,17 @@ const myChart = new Chart(ctx, {
     datasets: [{
       label: 'Land Use Area',
       data: values,    
-      backgroundColor: 'rgba(0,0,0,0.1)', 
-      borderColor: 'rgba(0,0,0,0.8)',
+      backgroundColor: barColors, 
+      borderColor: barColors,
       borderWidth: 1
     }]
   },
   options: { 
     responsive: true,
     maintainAspectRatio: false,
+    plugins: {
+      legend: { display: false } 
+    }
    }
 });
 
@@ -109,6 +167,8 @@ const myChart = new Chart(ctx, {
 function updateChart(dataArray, barangay) {
   myChart.data.labels = dataArray.map(p => p.class_name);
   myChart.data.datasets[0].data = dataArray.map(p => p.area_ha);
+  myChart.data.datasets[0].backgroundColor = dataArray.map(p => handleStyles(p.class_name).fillColor);
+  myChart.data.datasets[0].borderColor = dataArray.map(p => handleStyles(p.class_name).fillColor);    
   myChart.data.datasets[0].label = `${barangay}'s Land Use Area`;
   myChart.update();
 }
